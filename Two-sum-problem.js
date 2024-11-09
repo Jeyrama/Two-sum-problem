d87badcf04c3434d8f6d5b0a7077370a203e4069
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-09 15:36:38 -0800

    Create Two-sum-problem.js

---
+++ Two-sum-problem.js
@@ -0,0 +1,11 @@
+/*
+Write a function that takes an array of numbers (integers for the tests) and a target number. 
+It should find two different items in the array that, when added together, give the target value. 
+The indices of these items should then be returned in a list like so: (index1, index2).
+
+The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; 
+target will always be the sum of two different items from that array).
+
+twoSum([1, 2, 3], 4) // returns [0, 2] or [2, 0]
+twoSum([3, 2, 4], 6) // returns [1, 2] or [2, 1]
+*/
